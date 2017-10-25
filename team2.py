db09fc145561b2df978eaf927d0c466cbc612378
Aidan G <32966077+aidangraf13@users.noreply.github.com>
2017-10-25 13:22:06 -0500

    Update team2.py

--- team2.py
+++ team2.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'team2' # Only 10 chars displayed.
+strategy_name = 'tit for tat with randomness'
+strategy_description = 'The first'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
